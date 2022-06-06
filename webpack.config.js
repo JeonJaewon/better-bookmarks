83c3f757938aaf06014ad251274fc37ac8e3b241
Jaewon Jeon <wksk04515@naver.com>
2022-06-06 23:25:30 +0900

    Fix 초기 설정값 변경

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,14 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
-    popup: './src/Popup.tsx'
+    popup: './src/pages/Popup/index.tsx',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"]
+    extensions: ['.ts', '.tsx', '.js', 'jsx'],
   },
   module: {
     rules: [
@@ -20,9 +20,9 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader"
-          }
-        ]
+            loader: 'ts-loader',
+          },
+        ],
       },
       {
         test: /\.(js|jsx)$/,
@@ -30,21 +30,19 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: path.join(__dirname, 'src', 'pages', 'Popup', 'index.html'),
       filename: 'index.html',
     }),
     new CopyPlugin({
-      patterns: [
-        { from: "public" },
-      ],
-    })
+      patterns: [{ from: 'public' }],
+    }),
   ],
 };
