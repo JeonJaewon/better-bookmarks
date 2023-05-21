1eb81fc90efd045c62625c8befce62f66244cd63
Jeon Jaewon <wksk04515@naver.com>
2023-05-21 18:46:44 +0900

    Add husky, lint-staged (#7)
    * Add husky, lint-staged settings
    
    * FIx off eslint a11y settings
    
    * Fix types

--- src/pages/Popup/index.tsx
+++ src/pages/Popup/index.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { AppProvider } from '@src/providers/AppProvider';
-import Popup from './Popup';
+import { Popup } from '@src/pages/Popup/Popup';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
