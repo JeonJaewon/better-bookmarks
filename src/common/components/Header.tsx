1ccd0a07f17e260c2860f808aa2392e481dd9354
Jaewon Jeon <wksk04515@naver.com>
2023-05-26 04:07:53 +0900

    Add mantine `<Select />`

--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,19 +1,55 @@
 import { css, Theme, useTheme } from '@emotion/react';
-import SortIcon from '@public/svg/Sort.svg';
 import PlusIcon from '@public/svg/Plus.svg';
 import { useAddBookmarkModal } from '@src/features/Bookmark/hooks/useAddBookmarkModal';
-import { dateSortingOptionAtom } from '@src/features/Filter/atoms';
+import {
+  DateSortingOption,
+  dateSortingOptionAtom,
+} from '@src/features/Filter/atoms';
 import { useAtom } from 'jotai';
+import { Select } from '@mantine/core';
+
+const FILTER_OPTIONS: { value: DateSortingOption; label: string }[] = [
+  { value: 'older', label: 'Date Added - Ascending' },
+  { value: 'newer', label: 'Date Added - Descending' },
+];
+
+export const Header = () => {
+  const theme = useTheme();
+  const { openAddBookmarkModal } = useAddBookmarkModal();
+  const [dateSortingOption, setDateSortingOption] = useAtom(
+    dateSortingOptionAtom,
+  );
+
+  const handleChageSortOption = (value: DateSortingOption) => {
+    setDateSortingOption(value);
+  };
+
+  return (
+    <div css={styles.wrapper(theme)}>
+      <div css={styles.plusIcon} onClick={openAddBookmarkModal}>
+        <PlusIcon />
+      </div>
+      <div css={styles.sortIcon}>
+        <Select
+          placeholder="Sort By"
+          value={dateSortingOption}
+          onChange={handleChageSortOption}
+          data={FILTER_OPTIONS}
+        />
+      </div>
+    </div>
+  );
+};
 
 const styles = {
   wrapper: (theme: Theme) => {
     return css({
       display: 'flex',
       alignItems: 'center',
-      justifyContent: 'flex-end',
+      justifyContent: 'space-between',
       backgroundColor: theme.secondaryBackground,
       height: 60,
-      padding: '0 16px 0 0',
+      padding: '0 16px',
     });
   },
   plusIcon: css({
@@ -24,32 +60,3 @@ const styles = {
     '&:hover': {},
   }),
 };
-
-const Header = () => {
-  const theme = useTheme();
-  const [dateSortingOption, setDateSortingOption] = useAtom(
-    dateSortingOptionAtom,
-  );
-  const { openAddBookmarkModal } = useAddBookmarkModal();
-
-  const onClickSorted = () => {
-    if (dateSortingOption === 'newer') {
-      setDateSortingOption('older');
-    } else {
-      setDateSortingOption('newer');
-    }
-  };
-
-  return (
-    <div css={styles.wrapper(theme)}>
-      <div css={styles.plusIcon} onClick={openAddBookmarkModal}>
-        <PlusIcon />
-      </div>
-      <div css={styles.sortIcon} onClick={onClickSorted}>
-        <SortIcon />
-      </div>
-    </div>
-  );
-};
-
-export default Header;
